bb50395ee85743be95bb998203b53eecd731062f
Dev Kha <devkhanna@outlook.com>
2020-09-29 16:10:44 -0400

    index/script/style add

---
+++ script.js
@@ -0,0 +1,87 @@
+var generateBtn = document.querySelector("#generate");
+
+var chosenChar = [];
+
+var passwordLength;
+
+
+var lower = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ];
+var upper = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" ];
+var special = [ "!", "@", "#", "$", "%", "^", "&", "*", "+" ];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+// Assignment Code
+
+alert("Time to generate a new password!");
+
+
+generateBtn.addEventListener("click", passwordReqs);
+
+function passwordReqs() {
+    chosenChar = [];
+
+    alert("Follow the next prompts to create your password");
+
+     passwordLength = parseInt(prompt("Choose between 8 and 128 characters for your password length"));
+
+      if (passwordLength >= 8 && passwordLength <= 128) {
+     
+       
+       
+
+     addSpecial = confirm("Do you want any Special characters?");
+     addNumbers = confirm("Do you want any Numeric Characters?");
+     addLower = confirm("Do you want any Lowercase characters?");
+     addUpper = confirm("Do you want any Uppercase characters?");
+      }else { alert("not a valid password");
+           passwordReqs ();
+    }
+
+    // if (addSpecial || addNumbers || addLower || addUpper) {
+    //     alert("Your password meets all requirements!");
+    // }else { alert("Your password does meet all the requirements"); 
+    //    passwordReqs ();
+    //   }
+
+    if (addSpecial) {
+        chosenChar = chosenChar.concat(special);
+     }
+    if (addNumbers) {
+        chosenChar = chosenChar.concat(numbers);
+    }
+
+    if (addLower) {
+        chosenChar = chosenChar.concat(lower);
+    }
+
+    if (addUpper) {
+        chosenChar = chosenChar.concat(upper);
+    }
+
+    writePassword();
+}
+
+
+
+
+ function generatePassword() {
+    var password = "";
+    for (i = 0; i < passwordLength; i++) {
+        password = chosenChar[Math.floor(chosenChar.length * Math.random())];
+    }
+
+    return password;
+//     var passwordPool = [];
+//     console.log(passwordPool)
+    }
+
+
+// // Write password to the #password input
+   function writePassword() {
+     var password = generatePassword();
+     var passwordText = document.querySelector("#password");
+     passwordText.value = password;
+
+// //   passwordText.value = password; 
+   }
